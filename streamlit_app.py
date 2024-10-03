e3acf2b5d538d7e87dbda22449ccb629603d0e66
sarahdudek2W <128395607+sarahdudek2W@users.noreply.github.com>
2024-10-03 15:00:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
-session = cnx.ession()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -39,4 +39,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' +name_on_order+ '!', icon="✅")
+
 
